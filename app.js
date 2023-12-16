730b81ddcd6b6ccb18a19bcaa7dacd7e5e50edb7
Leonid Markovski <leonid.markovski96@gmail.com>
2023-12-16 03:56:28 +0100

    Adding a eventListener to the form and also adding perventDefault on the event.

---
+++ app.js
@@ -0,0 +1,15 @@
+	//Getting the DOM-elements with ID
+const inputEL=document.querySelector('input');
+const formEl=document.querySelector('form')
+
+
+	//Variables I need for now
+let NumberDogs=0;
+
+formEl.addEventListener('submit',e=>{
+	e.preventDefault()
+})
+
+
+
+
