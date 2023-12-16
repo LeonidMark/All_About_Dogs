f65340ef5c351a02454c89347a8da1ee8af21a3f
Leonid Markovski <leonid.markovski96@gmail.com>
2023-12-16 03:51:59 +0100

    Getting the data from the API using async-javascript

--- api.js
+++ api.js
@@ -1,1 +1,16 @@
 //Getting the API from a open API-Server
+const getData = async () =>{
+
+	try{
+		const response = await fetch("https://dog.ceo/api/breed/hound/images/random")
+		if(!response){
+			throw new Error('The API is now avalible')
+		}
+		const data = await response.json()
+		console.log(data)
+	} catch(err){
+		alert('Something went wrong with the data')
+
+	}
+}
+getData()
